b2e35d5f331234a5a68267d6f14756f534e780c2
SUNIL <sunilraghu199@gmail.com>
2023-01-18 10:09:06 +0530

    :lipstick: Added Color Schemes

--- lib/app/data/constants.dart
+++ lib/app/data/constants.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 const backgroundColor = Color(0xff101039);
+const foregroundColor = Color(0xff2d284d);
+const accentColor = Color(0xfffed058);
+const textcolor = Color(0xfffcfdfd);
